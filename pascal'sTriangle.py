a67f24ef464e4dcd24921e5c1d2e38b207ed5c7b
yju0044 <55807908+y0214ju@users.noreply.github.com>
2020-02-06 22:08:35 -0600

    Update pascal'sTriangle.py

--- pascal'sTriangle.py
+++ pascal'sTriangle.py
@@ -7,8 +7,7 @@ class Solution:
         result = [[1], [1,1]]
         current = [1,1]
         for i in range(numRows -2):
-                new = [current[j] + current[j+1] for j in range(len(current) -1)]
-                new = [1]+ new +[1]
+                new = [1]+ [current[j] + current[j+1] for j in range(len(current) -1)] +[1]
                 result.append(new)
                 current = new
                 
