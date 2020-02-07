d00d41397b6a2228a41d09afb701078c4342b038
yju0044 <55807908+y0214ju@users.noreply.github.com>
2020-02-06 22:07:06 -0600

    Create pascal'sTriangle.py

---
+++ pascal'sTriangle.py
@@ -0,0 +1,19 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        if(numRows == 0):
+            return [];
+        if(numRows == 1):
+            return [[1]]
+        result = [[1], [1,1]]
+        current = [1,1]
+        for i in range(numRows -2):
+                new = [current[j] + current[j+1] for j in range(len(current) -1)]
+                new = [1]+ new +[1]
+                result.append(new)
+                current = new
+                
+        return result
+            
+            
+            
+        
